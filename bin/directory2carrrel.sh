5b61517210b37882cb9dd1b20aa156a76bf79a5d
Eric L Morgan <emorgan@crcfe01.crc.nd.edu>
2018-07-19 08:43:09 -0400

    Adding additional functionalities

---
+++ bin/directory2carrrel.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+# directory2carrel.sh - given a directory, initialize a study carrel, cache/harvest content, and build the carrel
+
+# Eric Lease Morgan <emorgan@nd.edu>
+# (c) University of Notre Dame and distributed under a GNU Public License
+
+# July 19, 2018 - first cut
+
+
+# configure
+CACHE='cache';
+CARREL2ZIP='./bin/carrel2zip.pl'
+CARRELS='./carrels'
+HOME='/afs/crc.nd.edu/user/e/emorgan/local/reader'
+INITIALIZECARREL='./bin/initialize-carrel.sh'
+MAKE='./bin/make.sh'
+MAKENAME='./bin/make-name.sh'
+
+# validate input
+if [[ -z $1 ]]; then
+
+	echo "Usage: $0 <directory>" >&2
+	exit
+
+fi
+
+# get the input
+DIRECTORY=$1
+
+# make sane
+cd $HOME
+
+# initialize a (random) name
+NAME=$( $MAKENAME )
+
+# create a study carrel
+$INITIALIZECARREL $NAME
+
+# copy files accordingly; this will not recurse nor copy dot files
+cp $DIRECTORY/* "$CARRELS/$NAME/$CACHE"
+
+# build the carrel; the magic happens here
+$MAKE $NAME
+
+# zip it up
+$CARREL2ZIP $NAME
+
+# done
+echo $HOME/$NAME
+exit
---
+++ bin/file2carrrel.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+# file2carrel.sh - given a file, initialize a study carrel, cache/harvest content, and build the carrel
+
+# Eric Lease Morgan <emorgan@nd.edu>
+# (c) University of Notre Dame and distributed under a GNU Public License
+
+# July 19, 2018 - first cut
+
+
+# configure
+CACHE='cache';
+CARREL2ZIP='./bin/carrel2zip.pl'
+CARRELS='./carrels'
+HOME='/afs/crc.nd.edu/user/e/emorgan/local/reader'
+INITIALIZECARREL='./bin/initialize-carrel.sh'
+MAKE='./bin/make.sh'
+MAKENAME='./bin/make-name.sh'
+
+# validate input
+if [[ -z $1 ]]; then
+
+	echo "Usage: $0 <file>" >&2
+	exit
+
+fi
+
+# get the input
+FILE=$1
+
+# make sane
+cd $HOME
+
+# initialize a (random) name
+NAME=$( $MAKENAME )
+
+# create a study carrel
+$INITIALIZECARREL $NAME
+
+# copy the file; this will not recurse nor copy dot files
+cp $FILE "$CARRELS/$NAME/$CACHE"
+
+# build the carrel; the magic happens here
+$MAKE $NAME
+
+# zip it up
+$CARREL2ZIP $NAME
+
+# done
+echo $HOME/$NAME
+exit
