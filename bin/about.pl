b74f700beff951dfd7d0349d6b10d0eaff0102fa
Eric Lease Morgan <emorgan@nd.edu>
2019-06-11 15:17:27 +0000

    Working on about page

--- bin/about.pl
+++ bin/about.pl
@@ -2,21 +2,29 @@
 
 # about.pl - summarize the contents of a study carrel
 
+# Eric Lease Morgan <emorgan@nd.ed>
+# (c) University of Notre Dame; distributed under GNU Public License
+
+# June 11, 2019 - first documentation
+
+
+# configure
 use constant TEMPLATE => '/export/reader/etc/about.htm';
 
+# require; out-of-the-box Perl
 use strict;
 
 # number of items
 my $numberofitems = `../../bin/query.sh 'SELECT COUNT( id ) FROM bib;'`;
 chop( $numberofitems );
 
+# number of words
+my $sumofwords = `../../bin/query.sh 'SELECT SUM( words ) FROM bib;'`;
+chop( $sumofwords );
+
 # average size in words
 my $averagesizeinwords = `../../bin/query.sh 'SELECT ROUND(AVG( words ) ) FROM bib;'`;
 chop( $averagesizeinwords );
-
-# average readability score
-my $averagereadabilityscore = `../../bin/query.sh 'SELECT ROUND(AVG( flesch ) ) FROM bib;'`;
-chop( $averagereadabilityscore );
 
 # average readability score
 my $averagereadabilityscore = `../../bin/query.sh 'SELECT ROUND(AVG( flesch ) ) FROM bib;'`;
@@ -28,11 +36,33 @@ if ( ! -e './figures/sizes-boxplot.png' )    { `../../bin/plot-sizes.sh  boxplot
 if ( ! -e './figures/flesch-histogram.png' ) { `../../bin/plot-flesch.sh histogram ./figures/flesch-histogram.png` }
 if ( ! -e './figures/flesch-boxplot.png' )   { `../../bin/plot-flesch.sh boxplot   ./figures/flesch-boxplot.png`   }  
 
-# frequent ngrams
-my $frequentunigrams = `../../bin/ngrams.pl ./etc/reader.txt 1 | head -n 25 | cut -f1`;
-my @frequentunigrams = split /\n/, $frequentunigrams;
+# unigrams and their files
+my $frequentunigrams      = `../../bin/ngrams.pl ./etc/reader.txt 1 | head -n25 | cut -f1`;
+my @frequentunigrams      = split( /\n/, $frequentunigrams );
+my $pattern               = join( '\|', ( '\\b' . $frequentunigrams[ 0 ] . '\\b', '\\b' . $frequentunigrams[ 1 ] . '\\b', '\\b' . $frequentunigrams[ 2 ] . '\\b' ) );
+my $frequentunigramfiles  = `grep -Hice "$pattern" ./txt/*.txt | tr ':' ' ' | sort -rnk2 | head -n3 | cut -d ' ' -f1`;
+chop( $frequentunigramfiles );
+my @frequentunigramfiles  = split( /\n/, $frequentunigramfiles );
+my $frequentunigramfile01 = $frequentunigramfiles[ 0 ];
+my $frequentunigramfile02 = $frequentunigramfiles[ 1 ];
+my $frequentunigramfile03 = $frequentunigramfiles[ 2 ];
+
+# bigrams and their files
 my $frequentbigrams  = `../../bin/ngrams.pl ./etc/reader.txt 2 | head -n 25 | cut -f1`;
 my @frequentbigrams  = split /\n/, $frequentbigrams;
+my $pattern               = join( '\|', ( $frequentbigrams[ 0 ], $frequentbigrams[ 1 ], $frequentbigrams[ 2 ] ) );
+my $frequentbigramfiles  = `grep -Hice "$pattern" ./txt/*.txt | tr ':' ' ' | sort -rnk2 | head -n3 | cut -d ' ' -f1`;
+chop( $frequentbigramfiles );
+my @frequentbigramfiles  = split( /\n/, $frequentbigramfiles );
+my $frequentbigramfile01 = $frequentbigramfiles[ 0 ];
+my $frequentbigramfile02 = $frequentbigramfiles[ 1 ];
+my $frequentbigramfile03 = $frequentbigramfiles[ 2 ];
+
+# plot the respective word clouds
+`../../bin/ngrams.pl ./etc/reader.txt 1 | head -n 150 > ./tmp/unigrams.tsv`;
+if ( ! -e './figures/unigrams.png' ) { `../../bin/cloud.py ./tmp/unigrams.tsv white ./figures/unigrams.png` }
+`../../bin/ngrams.pl ./etc/reader.txt 2 | head -n 150 > ./tmp/bigrams.tsv`;
+if ( ! -e './figures/bigrams.png' ) { `../../bin/cloud.py ./tmp/bigrams.tsv white ./figures/bigrams.png`; }
 
 # topic model with a single word and single file
 my $model  = `../../bin/topic-model.py ./txt 1 1`;
@@ -54,12 +84,6 @@ my ( $topicsquin02, $topicsquinfile02 ) = split( "\t", @$topics[ 1 ] );
 my ( $topicsquin03, $topicsquinfile03 ) = split( "\t", @$topics[ 2 ] );
 my ( $topicsquin04, $topicsquinfile04 ) = split( "\t", @$topics[ 3 ] );
 my ( $topicsquin05, $topicsquinfile05 ) = split( "\t", @$topics[ 4 ] );
-
-# plot the respective word clouds
-`../../bin/ngrams.pl ./etc/reader.txt 1 | head -n 150 > ./tmp/unigrams.tsv`;
-if ( ! -e './figures/unigrams.png' ) { `../../bin/cloud.py ./tmp/unigrams.tsv white ./figures/unigrams.png` }
-`../../bin/ngrams.pl ./etc/reader.txt 2 | head -n 150 > ./tmp/bigrams.tsv`;
-if ( ! -e './figures/bigrams.png' ) { `../../bin/cloud.py ./tmp/bigrams.tsv white ./figures/bigrams.png`; }
 
 # get top 25 nouns
 my $nouns = `../../bin/query.sh 'SELECT lemma FROM pos where pos like "N%%" GROUP BY lemma ORDER BY COUNT( lemma ) DESC LIMIT 25;'`;
@@ -89,7 +113,7 @@ my @adverbs = split /\n/, $adverbs;
 my $keywords = `../../bin/query.sh 'SELECT keyword FROM wrd GROUP BY keyword ORDER BY COUNT( keyword ) DESC LIMIT 2;'`;
 my @keywords = split /\n/, $keywords;
 $keywords = '\b' . join( '\b|\b', @keywords ) . '\b';
-my $concordance = `../../bin/concordance.pl ./etc/reader.txt '$keywords' | shuf -n 50`;
+my $concordance = `../../bin/concordance.pl ./etc/reader.txt '$keywords' | shuf -n 25`;
 
 # get top 7 keywords
 my $keywords = `../../bin/query.sh 'SELECT keyword FROM wrd GROUP BY keyword ORDER BY COUNT( keyword ) DESC LIMIT 25;'`;
@@ -114,6 +138,7 @@ close HTML;
 
 # do the substitutions
 $html =~ s/##NUMBEROFITEMS##/$numberofitems/;
+$html =~ s/##SUMOFWORDS##/$sumofwords/;
 $html =~ s/##AVERAGESIZEINWORDS##/$averagesizeinwords/;
 $html =~ s/##AVERAGEREADABILITSCORE##/$averagereadabilityscore/;
 $html =~ s/##FREQUENTUNIGRAMS##/join( ', ', @frequentunigrams )/e;
@@ -144,6 +169,12 @@ $html =~ s/##TOPICSQUINFILE02##/$topicsquinfile02/eg;
 $html =~ s/##TOPICSQUINFILE03##/$topicsquinfile03/eg;
 $html =~ s/##TOPICSQUINFILE04##/$topicsquinfile04/eg;
 $html =~ s/##TOPICSQUINFILE05##/$topicsquinfile05/eg;
+$html =~ s/##FREQUENTUNIGRAMFILE01##/$frequentunigramfile01/eg;
+$html =~ s/##FREQUENTUNIGRAMFILE02##/$frequentunigramfile02/eg;
+$html =~ s/##FREQUENTUNIGRAMFILE03##/$frequentunigramfile03/eg;
+$html =~ s/##FREQUENTBIGRAMFILE01##/$frequentbigramfile01/eg;
+$html =~ s/##FREQUENTBIGRAMFILE02##/$frequentbigramfile02/eg;
+$html =~ s/##FREQUENTBIGRAMFILE03##/$frequentbigramfile03/eg;
 
 # output and done
 print $html;
