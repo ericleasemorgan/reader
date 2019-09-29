ab2c058a8eae10f87a53573c40f4233b736387da
Eric Lease Morgan <emorgan@nd.edu>
2019-09-29 19:47:21 +0000

    Updating bin directory

--- bin/initialize-carrel.sh
+++ bin/initialize-carrel.sh
@@ -5,10 +5,11 @@
 # Eric Lease Morgan <emorgan@nd.edu>
 # (c) University of Notre Dame and distributed under a GNU Public License
 
-# July 14, 2018 - first cut
-# July 20, 2018 - added log directory
-# July 26, 2018 - added home page
-# May  24, 2019 - added files for indexing and searching
+# July      14, 2018 - first cut
+# July      20, 2018 - added log directory
+# July      26, 2018 - added home page
+# May       24, 2019 - added files for indexing and searching
+# September 29, 2019 - added some file for study carrel investigation
 
 
 # configure
@@ -47,36 +48,41 @@ mkdir -p "./lib"
 mkdir -p "./wrd"
 
 # fill file system
-cp -r ../../etc/Lingua            "./etc"
-cp ../../bin/about.pl             "./bin"
-cp ../../bin/classify.pl          "./bin"
-cp ../../bin/cluster.py           "./bin"
-cp ../../bin/concordance.pl       "./bin"
-cp ../../bin/db2solr.pl           "./bin"
-cp ../../bin/keyword2sentences.pl "./bin"
-cp ../../bin/lines2files.pl       "./bin"
-cp ../../bin/list-questions.pl    "./bin"
-cp ../../bin/ngrams.pl            "./bin"
-cp ../../bin/query.sh             "./bin"
-cp ../../bin/search-solr.pl       "./bin"
-cp ../../bin/search-vec.py        "./bin"
-cp ../../bin/topic-model.py       "./bin"
-cp ../../cgi-bin/concordance.cgi  "./cgi-bin"
-cp ../../cgi-bin/search-solr.cgi  "./cgi-bin"
-cp ../../css/*                    "./css"
-cp ../../etc/about.htm            "./etc"
-cp ../../etc/home.html            "./"
-cp ../../etc/LICENSE              "./"
-cp ../../etc/queries.sql          "./etc"
-cp ../../etc/reader.sql           "./etc"
-cp ../../etc/README               "./"
-cp ../../etc/schema.xml           "./etc"
-cp ../../etc/stopwords.txt        "./etc"
-cp ../../etc/style.css            "./etc"
-cp ../../etc/tfidf-toolbox.pl     "./etc"
-cp ../../js/*                     "./js"
-cp ../../lib/*.js                 "./lib"
-cp ../../lib/*.css                "./lib"
+cp -r ../../etc/Lingua                  "./etc"
+cp ../../bin/about.pl                   "./bin"
+cp ../../bin/classify.pl                "./bin"
+cp ../../bin/cluster.py                 "./bin"
+cp ../../bin/concordance.pl             "./bin"
+cp ../../bin/list-items.sh              "./bin"
+cp ../../bin/make-mallet-metadata.sh    "./bin"
+cp ../../bin/make-subset.sh             "./bin"
+cp ../../bin/mallet-visualize-pivot.py  "./bin"
+cp ../../bin/mallet-visualize.py        "./bin"
+cp ../../bin/db2solr.pl                 "./bin"
+cp ../../bin/keyword2sentences.pl       "./bin"
+cp ../../bin/lines2files.pl             "./bin"
+cp ../../bin/list-questions.pl          "./bin"
+cp ../../bin/ngrams.pl                  "./bin"
+cp ../../bin/query.sh                   "./bin"
+cp ../../bin/search-solr.pl             "./bin"
+cp ../../bin/search-vec.py              "./bin"
+cp ../../bin/topic-model.py             "./bin"
+cp ../../cgi-bin/concordance.cgi        "./cgi-bin"
+cp ../../cgi-bin/search-solr.cgi        "./cgi-bin"
+cp ../../css/*                          "./css"
+cp ../../etc/about.htm                  "./etc"
+cp ../../etc/home.html                  "./"
+cp ../../etc/LICENSE                    "./"
+cp ../../etc/queries.sql                "./etc"
+cp ../../etc/reader.sql                 "./etc"
+cp ../../etc/README                     "./"
+cp ../../etc/schema.xml                 "./etc"
+cp ../../etc/stopwords.txt              "./etc"
+cp ../../etc/style.css                  "./etc"
+cp ../../etc/tfidf-toolbox.pl           "./etc"
+cp ../../js/*                           "./js"
+cp ../../lib/*.js                       "./lib"
+cp ../../lib/*.css                      "./lib"
 
 echo "Initializing database" >&2
 $INITIALIZEDB "$CARRELS/$NAME"
