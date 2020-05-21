4c9910cef88a7a818a24395854cfc0a076bf52d0
Eric Lease Morgan <emorgan@nd.edu>
2020-05-21 12:36:02 +0000

    Started using faceted search

--- bin/search.pl
+++ bin/search.pl
@@ -5,12 +5,13 @@
 # Eric Lease Morgan <emorgan@nd.edu>
 # April 30, 2019 - first cut; based on earlier work
 # May    2, 2019 - added classification and files (urls)
-# July 8, 2019 - added author, title, and date
+# July   8, 2019 - added author, title, and date
+# May   21, 2019 - migrating to Reader CORD
 
 
 # configure
-use constant FACETFIELD => ( );
-use constant SOLR       => 'http://localhost:8983/solr/cord';
+use constant FACETFIELD => ( 'facet_journal', 'year' );
+use constant SOLR       => 'http://localhost:8984/solr/covid19';
 use constant ROWS       => 999;
 
 # require
@@ -24,15 +25,27 @@ if ( ! $type || ! $query ) { die "Usage: $0 <list|SQL> <query>\n" }
 
 # initialize
 my $solr   = WebService::Solr->new( SOLR );
+binmode( STDOUT, ':utf8' );
 
 # build the search options
 my %search_options = ();
 $search_options{ 'facet.field' } = [ FACETFIELD ];
+$search_options{ 'fl' }          = 'id,title,doi,url,date,journal';
 $search_options{ 'facet' }       = 'true';
 $search_options{ 'rows' }        = ROWS;
 
 # search
 my $response = $solr->search( "$query", \%search_options );
+
+# build a list of journal facets
+my @facet_journal = ();
+my $journal_facets = &get_facets( $response->facet_counts->{ facet_fields }->{ facet_journal } );
+foreach my $facet ( sort { $$journal_facets{ $b } <=> $$journal_facets{ $a } } keys %$journal_facets ) { push @facet_journal, $facet . ' (' . $$journal_facets{ $facet } . ')'; }
+
+# year
+my @facet_year = ();
+my $year_facets = &get_facets( $response->facet_counts->{ facet_fields }->{ year } );
+foreach my $facet ( sort { $$year_facets{ $b } <=> $$year_facets{ $a } } keys %$year_facets ) { push @facet_year, $facet . ' (' . $$year_facets{ $facet } . ')'; }
 
 # get the total number of hits
 my $total = $response->content->{ 'response' }->{ 'numFound' };
@@ -44,19 +57,29 @@ if ( $type eq 'list' ) {
 
 	# start the output
 	print "Your search found $total item(s) and " . scalar( @hits ) . " item(s) are displayed.\n\n";
+	print '     year facets: ', join( '; ', @facet_year ), "\n\n";
+	print '  journal facets: ', join( '; ', @facet_journal ), "\n\n";
 
 	# loop through each document
 	for my $doc ( $response->docs ) {
 
 		# parse
-		my $document_id = $doc->value_for( 'document_id' );
+		my $document_id = $doc->value_for( 'id' );
 		my $title       = $doc->value_for( 'title' );
+		my $date        = $doc->value_for( 'date' );
 		my $abstract    = $doc->value_for( 'abstract' );
+		my $journal     = $doc->value_for( 'journal' );
+		my $url         = $doc->value_for( 'url' );
+		my $doi         = $doc->value_for( 'doi' );
 	
 		# output
 		print "  document id: $document_id\n";
 		print "        title: $title\n";
-		print "     abstract: $abstract\n";
+		#print "     abstract: $abstract\n";
+		print "      journal: $journal\n";
+		print "          url: $url\n";
+		print "          doi: $doi\n";
+		print "         date: $date\n";
 		print "\n";
 
 	}
@@ -72,7 +95,7 @@ elsif ( $type eq 'sql' ) {
 	for my $doc ( $response->docs ) {
 
 		# parse
-		my $document_id = $doc->value_for( 'document_id' );
+		my $document_id = $doc->value_for( 'id' );
 		push( @document_ids, "document_id='$document_id'" )
 		
 	}
@@ -87,4 +110,26 @@ else { die "Usage: $0 <list|SQL> <query>\n" }
 
 # done
 exit;
+
+
+# convert an array reference into a hash
+sub get_facets {
+
+	my $array_ref = shift;
+	
+	my %facets;
+	my $i = 0;
+	foreach ( @$array_ref ) {
+	
+		my $k = $array_ref->[ $i ]; $i++;
+		my $v = $array_ref->[ $i ]; $i++;
+		next if ( ! $v );
+		$facets{ $k } = $v;
+	 
+	}
+	
+	return \%facets;
+	
+}
+
 
