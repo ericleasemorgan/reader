3ee460d5b3d4f8ff33e27c3f06b3df42d17548df
Ralph LeVan <ralphlevan@gmail.com>
2020-07-08 20:10:56 +0000

    Added link for solrCloudHostList.txt

--- bin/linkReaderCloneToSolrCloudFiles.sh
+++ bin/linkReaderCloneToSolrCloudFiles.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 echo Hi Ralph!
 ln -sfn /export/solr/solr-cloud-branch/reader/bin/linkReaderCloneToSolrCloudFiles.sh /export/coredir/
+ln -sfn /export/solr/solr-cloud-branch/reader/bin/solrCloudHostlist.txt /export/coredir/
