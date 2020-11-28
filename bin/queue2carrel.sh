9302e289bd7e662dd53179e241e22d80cec8d7ca
Eric Lease Morgan <eric_morgan@infomotions.com>
2020-11-28 13:46:02 +0000

    Edited to actually submit the work

--- bin/queue2carrel.sh
+++ bin/queue2carrel.sh
@@ -5,25 +5,28 @@
 # Eric Lease Morgan <emorgan@nd.edu>
 # (c) University of Notre Dame; distributed under a GNU Public License
 
-# May  28, 2020 - first cut; while Gus is literally on his last legs
-# June  2, 2020 - added provenance
-# June  3, 2020 - added partition
-# July 31, 2020 - added exit if carrel already exists; will no longer overwrite, hmmm
+# May      28, 2020 - first cut; while Gus is literally on his last legs
+# June      2, 2020 - added provenance
+# June      3, 2020 - added partition
+# July     31, 2020 - added exit if carrel already exists; will no longer overwrite, hmmm
+# November 28, 2020 - made to work with Azure; on Thanksgiving vacation in Lancaster during a pandemic
 
 
 # configure
 SEARCH2CARREL='search2carrel.sh'
 CARRELS="$READERCORD_HOME/carrels"
-CARRELSUBMIT='carrel-submit.sh'
+CORD2CARREL='cord2carrel.sh'
+#CARRELSUBMIT='carrel-submit.sh'
 
 # sanity check
 if [[ -z $1 ]]; then 
 	echo "Usage: $0 <file>" >&2
 	exit
 fi
-
-# get input
 FILE=$1
+
+# make sane
+cd $READERCORD_HOME
 
 # initialize
 IFS=$'\t'
@@ -51,6 +54,11 @@ cat $FILE | while read SHORTNAME DATE TIME EMAIL QUERY; do
 	# for reasons of provenance, copy the queue file to the carrel
 	cp $FILE "$CARRELS/$SHORTNAME/provenance.tsv"
 	
+	# make sane and do the work
+	echo "Making sane and doing the work ($CORD2CARREL)" >&2
+	cd "$CARRELS/$SHORTNAME"
+	$CORD2CARREL 1>standard-output.txt 2>standard-error.txt &
+
 	# create a slurm script and submit it; do the work
 	#$CARRELSUBMIT $SHORTNAME $PARTITION $EMAIL
 	
